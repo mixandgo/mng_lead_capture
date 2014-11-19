f7f076e614ff54f2aad27412b60f9f025513ad69
Marcu Alexandru <amarcu@repairpal.com>
2014-11-19 15:56:15 +0200

    fixed the previous cucumbers

--- app/models/lead_signup.rb
+++ app/models/lead_signup.rb
@@ -1,5 +1,4 @@
 class LeadSignup < ActiveRecord::Base
   validates :name, :presence => true
-  validates :email, :presence => true, :uniqueness => true, :format => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
-  validates :phone, :format => /[\(\)0-9\- \+\.]{10,20} *[extension\.]{0,9} *[0-9]{0,5}/, :allow_nil => true, :allow_blank => true
+  validates :email, :presence => true, :uniqueness => true
 end
--- spec/models/lead_signup_spec.rb
+++ spec/models/lead_signup_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe LeadSignup, :type => :model do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:email) }
   it { should validate_uniqueness_of(:email) }
-  it { should allow_value("jhondoe@example.com").for(:email )}
-  it { should_not allow_value("noemail").for(:email) }
-  it { should allow_value(nil, "", "123-123-3213", "1234567890", "(1234)1234456", "1234 123 345", "1234.123.123").for(:phone) }
-  it { should_not allow_value("a", "123").for(:phone) }
 end
